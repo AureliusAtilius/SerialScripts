36bbd0463edda6bbc7c26394ce9efb9735c9c29c
Josh <j.j.h.lindner@gmail.com>
2021-09-05 21:59:23 -0700

    Create command file.
    Edit to send commands to device read from command file.

--- juniper_checkout.py
+++ juniper_checkout.py
@@ -1,8 +1,6 @@
 #! usr/bin/env python
-import serial
+import serial, sys
 from serial.serialutil import STOPBITS_ONE
-
-import serial
 from time import sleep
  
  
@@ -11,18 +9,29 @@ def send_to_console(ser: serial.Serial, command: str, wait_time: float = 0.5):
     ser.write(command_to_send.encode('utf-8'))
     sleep(wait_time)
     print(ser.read(ser.inWaiting()).decode('utf-8'), end="")
- 
-with serial.Serial("COM5", timeout=1) as ser:
-    print(f"Connecting to {ser.name}...")
-    send_to_console(ser, "")
-    send_to_console(ser, "username")
-    send_to_console(ser, "pw")
-    send_to_console(ser, "cli")
-    send_to_console(ser, "show version", wait_time=2)
-    send_to_console(ser, "show chassis hardware", wait_time=2)
-    send_to_console(ser, "show chassis environment", wait_time=2)
-    send_to_console(ser, "show chassis fpc")
-    send_to_console(ser, "show chassis fpc pic-status")
-    send_to_console(ser, "exit")
-    send_to_console(ser, "exit")
-    print(f"Connection to {ser.name} closed.")
+
+def send_to_console_write(ser: serial.Serial, vendor=sys.argv(1), wait_time: float = 0.5, filename=sys.argv(2), ):
+    with open(filename) as commands:
+        for line in commands.readlines:
+            send_to_console(ser, line)
+    sleep(wait_time)
+    output=(ser.read(ser.inWaiting()).decode('utf-8'))
+    with open(filename,"a+") as file:
+        file.write(output)
+
+
+def main(): 
+    with serial.Serial("COM5", timeout=1) as ser:
+        print(f"Connecting to {ser.name}...")
+        send_to_console(ser, "")
+        send_to_console(ser, "username")
+        send_to_console(ser, "pw")
+        send_to_console(ser, "cli")
+        send_to_console_write(ser, "show version", wait_time=2)
+        send_to_console(ser, "exit")
+        send_to_console(ser, "exit")
+        print(f"Connection to {ser.name} closed.")
+
+
+if __name__=="__main__":
+    main()
