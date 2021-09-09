cd5737c78df0b0d42e1ddcae15270e9bc6c82057
AureliusAtilius <j.j.h.lindner@gmail.com>
2021-09-09 12:18:45 -0700

    Add input validation for vendor.
    Add filepath to command files.

--- checkout.py
+++ checkout.py
@@ -1,5 +1,5 @@
 #! usr/bin/env python
-import serial, sys
+import serial
 from serial.serialutil import STOPBITS_ONE
 from time import sleep
  
@@ -11,12 +11,12 @@ def send_command(ser: serial.Serial, command: str, wait_time: float = 1):
     print(ser.read(ser.inWaiting()).decode('utf-8'), end="")
 
 def login(vendor):
-    with open("\\{}\\default_login.txt")as file:
+    with open("\\{}\\default_login.txt".format(vendor))as file:
         for line in file.readlines():
             send_command(line)
 
-def send_command_write(ser: serial.Serial, vendor, wait_time: float = 0.5, filename=sys.argv(2), ):
-    with open(filename) as commands:
+def send_command_write(ser: serial.Serial, vendor, filename, wait_time: float = 0.5):
+    with open('\\{}\\checkout.txt'.format(vendor)) as commands:
         for line in commands.readlines:
             send_command(ser, line)
     sleep(wait_time)
@@ -27,14 +27,18 @@ def send_command_write(ser: serial.Serial, vendor, wait_time: float = 0.5, filen
 
 def main():
     vendors={"1":"Juniper","2":"Cisco","3":"HPE","4":"Dell"}
-    vendor=input("Select Vendor(1-4): \b1.Juniper \b2.Cisco \b3.HPE \b4.Dell\b")
-
-    file_name=input("Checkout File Name: \b") 
+    while True:
+        vendor=input("Select Vendor(1-4): \n1.Juniper \n2.Cisco \n3.HPE \n4.Dell\n")
+        if vendor not in vendors.keys():
+            continue
+        else:
+            break
+    file_name=input("Checkout File Name: \n") 
     with serial.Serial("COM5", timeout=1) as ser:
         print(f"Connecting to {ser.name}...")
         send_command(ser, "")
         login(vendors[vendor])
-        send_command_write(ser, vendors[vendor],wait_time=2)
+        send_command_write(ser, vendors[vendor],file_name,wait_time=2)
         send_command(ser, "exit")
         send_command(ser, "exit")
         print(f"Connection to {ser.name} closed.")
